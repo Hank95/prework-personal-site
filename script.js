260e63d05f5bc7b646c49cfb2477ef4e4a0c9c55
Henry Pendleton <hhpendleton@gmail.com>
2021-05-27 22:01:53 -0400

    Dark mode

--- script.js
+++ script.js
@@ -5,10 +5,6 @@ const burger = document.querySelector("#burger-menu");
 const arrow = document.querySelector("#arrow");
 const menu = document.querySelector("#cont");
 
-console.log(burger);
-console.log(arrow);
-console.log(menu);
-
 const openMenu = function (e) {
   e.preventDefault();
   menu.classList.remove("hidden");
@@ -24,3 +20,31 @@ const closeMenu = function () {
 burger.addEventListener("click", openMenu);
 
 arrow.addEventListener("click", closeMenu);
+
+// Night Mode
+
+const moon = document.querySelector(".moon");
+const nightModeDiv = document.querySelector(".night-mode");
+const sun = document.querySelector(".sun");
+const dayModeDiv = document.querySelector(".day-mode");
+const backgroundImage = document.querySelector("#background");
+const html = document.querySelector("html");
+const landing = document.querySelector("#landing");
+const logo = document.querySelector("#logo");
+const logoFoot = document.querySelector("#foot-logo");
+
+console.log(backgroundImage);
+console.log(logo);
+const nightMode = function (e) {
+  e.preventDefault();
+  nightModeDiv.classList.add("hidden");
+  dayModeDiv.classList.remove("hidden");
+  backgroundImage.src = "./images/ocean-texture-dark.jpg";
+  html.style.color = "white";
+  html.style.backgroundColor = "rgb(18,18,18)";
+  landing.style.color = "white";
+  logo.src = "./images/hp_logo2_white.svg";
+  logoFoot.src = "./images/hp_logo2_white.svg";
+};
+
+moon.addEventListener("click", nightMode);
